4a41391bf9d5ba77ba803c0308de0eaf4ecd52dc
sindyvanessamelo <sindyprieto845@gmail.com>
2020-08-15 22:46:31 -0500

    desarrollo cuarto programa

---
+++ clase 1/programa 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace programa_4
+{
+    class Program
+    {
+        //escribir un algoritmo que permita calcular el radio de un círculo,teniendo el área//
+
+        static void Main(string[] args)
+        {
+            float pi = 3.1416f;
+            float area = 0.0f;
+            string entrada = "";
+
+
+            Console.WriteLine("hola, hoy vamos a calcular el radio de un círculo");
+            Console.WriteLine("");
+            Console.WriteLine("por favor digite el valor del area:");
+            entrada = Console.ReadLine();
+            area = Convert.ToSingle(entrada);
+            Console.WriteLine("");
+            var bs = Math.Sqrt(area / pi);
+            Console.WriteLine("el valor del area es de:{0} por lo tanto el valor de el radio del circulo es de:{1}",area,bs);
+
+
+        }
+    }
+}
