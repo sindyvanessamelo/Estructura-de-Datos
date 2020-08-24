f32a0e9cc4e0b7c6ffc93a5ef8bfc31b2d0f3b91
sindyvanessamelo <sindyprieto845@gmail.com>
2020-08-23 20:41:27 -0500

    desarrollo ejercicios

--- clase 2/programa 3/Program.cs
+++ clase 2/programa 3/Program.cs
@@ -1,44 +1,33 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
-namespace programa2prct2
+namespace programa3prct2
 {
     class Program
-    { /* escribir el algoritmo que permita emitir la factura correspondiente a una compra de un artículo determinado del que se adquieren una o varias unidades. 
-       * El IVA a aplicar es del 11.5% y si el precio bruto (precio de venta más IVA) es mayor de 500 Bs.F., se aplicará un descuento del 6.5%.*/
+    {
         static void Main(string[] args)
         {
+            int n, i, c;
+            c = 0;
+            Console.WriteLine("Ingresa Numero");
+            n = int.Parse(Console.ReadLine());
 
-            float vv=0.0f, iva = 0.0f, pv = 0.0f, preciototal=0.0f;
-            float desc = 0.065f;
-            string articulo = "", entrada = "";
-            int unidades = 0;
-
-            System.Console.WriteLine("¿cuál artículo comprará?   ");
-            entrada = System.Console.ReadLine();
-            articulo = new string(entrada);
-            
-            System.Console.WriteLine("Digita por favor el valor del articulo =  ");
-            vv = float.Parse(System.Console.ReadLine());
-
-            System.Console.WriteLine(" ¿cuantás unidades comprará? ");
-            unidades = int.Parse(System.Console.ReadLine());
-            
-            iva = vv * 0.115f;
-            System.Console.WriteLine("valor iva= {0} ", iva);
-        
-        pv = vv + iva;
-
-            if (pv >= 500)
+            for (i = 1; i <= n; i++)
             {
-                desc = 0.065f * vv;
-                preciototal = vv - desc;
-                System.Console.WriteLine("El precio de venta es= {0} pesos, más un descuento = {1} pesos, por lo tanto el total a pagar es de: {2} pesos" , pv,desc,preciototal);
+                if (n % i == 0)
+                {
+                    c = c + 1;
+                }
             }
-           else
+            if (c > 2)
             {
-                System.Console.WriteLine("El precio de venta es= {0} " , pv);
+                Console.WriteLine(" el número {0} No es primo ", n);
             }
+            else
+            {
+                Console.WriteLine("el número {0} Es primo", n);
+            }
+            
+
         }
     }
 }
---
+++ clase 2/programa 8/Clase2Ejercicio8.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Clase2Ejercico8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int D, M, A;
+            Console.WriteLine("Digite el Día: ");
+            D = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el Mes: ");
+            M = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el Año: ");
+            A = int.Parse(Console.ReadLine());
+
+            if (D >= 1 && D <= 30)
+            {
+                D = D + 1;
+                Console.WriteLine("La fecha es: {0}/{1}/{2}", D, M, A);
+            }
+            if (D > 31)
+            {
+                Console.WriteLine("Esa fecha no existe");
+            }
+            if (D == 31 && M <= 11)
+            {
+                D = D - 30;
+                M = M + 1;
+
+                Console.WriteLine("La fecha es: {0}/{1}/{2}", D, M, A);
+            }
+            if (M > 12)
+            {
+                Console.WriteLine("Esa fecha no existe {0}", M);
+            }
+            if (M == 12 && D == 31)
+            {
+                D = D - 30;
+                M = M - 11;
+                A = A + 1;
+                Console.WriteLine("La fecha es: {0}/{1}/{2}", D, M, A);
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
+
