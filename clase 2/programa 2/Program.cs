871504c8739c28f0b01e8653060bc7bb891a8a82
sindyvanessamelo <sindyprieto845@gmail.com>
2020-08-23 20:10:42 -0500

    desarrollo ejercicios clase 2

---
+++ clase 2/programa 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace programa3prct2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, c;
+            c = 0;
+            Console.WriteLine("Ingresa Numero");
+            n = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    c = c + 1;
+                }
+            }
+            if (c > 2)
+            {
+                Console.WriteLine(" el número {0} No es primo ", n);
+            }
+            else
+            {
+                Console.WriteLine("el número {0} Es primo", n);
+            }
+            
+
+        }
+    }
+}
---
+++ clase 2/programa 3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace programa2prct2
+{
+    class Program
+    { /* escribir el algoritmo que permita emitir la factura correspondiente a una compra de un artículo determinado del que se adquieren una o varias unidades. 
+       * El IVA a aplicar es del 11.5% y si el precio bruto (precio de venta más IVA) es mayor de 500 Bs.F., se aplicará un descuento del 6.5%.*/
+        static void Main(string[] args)
+        {
+
+            float vv=0.0f, iva = 0.0f, pv = 0.0f, preciototal=0.0f;
+            float desc = 0.065f;
+            string articulo = "", entrada = "";
+            int unidades = 0;
+
+            System.Console.WriteLine("¿cuál artículo comprará?   ");
+            entrada = System.Console.ReadLine();
+            articulo = new string(entrada);
+            
+            System.Console.WriteLine("Digita por favor el valor del articulo =  ");
+            vv = float.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine(" ¿cuantás unidades comprará? ");
+            unidades = int.Parse(System.Console.ReadLine());
+            
+            iva = vv * 0.115f;
+            System.Console.WriteLine("valor iva= {0} ", iva);
+        
+        pv = vv + iva;
+
+            if (pv >= 500)
+            {
+                desc = 0.065f * vv;
+                preciototal = vv - desc;
+                System.Console.WriteLine("El precio de venta es= {0} pesos, más un descuento = {1} pesos, por lo tanto el total a pagar es de: {2} pesos" , pv,desc,preciototal);
+            }
+           else
+            {
+                System.Console.WriteLine("El precio de venta es= {0} " , pv);
+            }
+        }
+    }
+}
