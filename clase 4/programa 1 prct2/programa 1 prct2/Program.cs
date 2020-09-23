7f81070afdbe8435bc0b5386f9881c27452b417d
sindyvanessamelo <sindyprieto845@gmail.com>
2020-09-22 22:13:53 -0500

    desarrollo practica 5

---
+++ clase 4/programa 1 prct2/programa 1 prct2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programa_1_prct2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
